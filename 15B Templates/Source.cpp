cf9301b1df201b6778b06473ed469a7c4f47304d
larralu <larralu@aol.com>
2021-12-05 20:33:38 -0500

    t half done

---
+++ 15B Templates/Source.cpp
@@ -0,0 +1,18 @@
+//Laura Luke
+//12-05-2021
+//CIS 1202 501
+//15B Templates
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+template <typename T>
+
+T half(T value)
+{
+	return value / 2;
+}
+
