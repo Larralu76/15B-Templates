0ec0dbff206258bf9cd93e4b58260e6bec222c3c
larralu <larralu@aol.com>
2021-12-05 20:41:37 -0500

    completed main

--- 15B Templates/Source.cpp
+++ 15B Templates/Source.cpp
@@ -22,3 +22,15 @@ int half(int value)
 	else return (int)(value / 2) + 1;
 }
  
+int main()
+{
+    double a = 7.0;
+    float b = 5.0f;
+    int c = 3;
+
+    cout << half(a) << endl;
+    cout << half(b) << endl;
+    cout << half(c) << endl;
+
+    return 0;
+}
